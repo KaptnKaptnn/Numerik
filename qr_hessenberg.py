4c09c0a655c44f8301596f1218cd75811b28dd4a
MarlonSchnell <118753106+MarlonSchnell@users.noreply.github.com>
2023-06-16 02:47:25 +0200

    Add new functionalities and graph data

--- qr_hessenberg.py
+++ qr_hessenberg.py
@@ -1,27 +1,36 @@
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
+def timing_decorator(func):
+    execution_times = []
+    
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        execution_times.append(execution_time)
+        return result
 
-def recursive_calc(m, n):
-    a = 0
+    wrapper.execution_times = execution_times
+    return wrapper
 
-    if m <= n + 1:
-        a = 1 / (m + n - 1)
-    else:
-        a = 0
-
-    return a
-
-
+@timing_decorator
 def assemble_matrix(size):
     matrix = np.zeros(shape=(size, size))
 
     for i in range(1, size):
         for j in range(1, size):
-            matrix[i][j] = recursive_calc(i, j)
+
+            if(i <= j + 1):
+                matrix[i][j] = 1 / (i + j - 1)
+            else:
+                matrix[i][j] = 0
 
     return matrix
 
-
+@timing_decorator
 def assemble_vector(size):
     vector = np.zeros(shape=(size))
 
@@ -29,38 +38,72 @@ def assemble_vector(size):
         result = 0
 
         for n in range(1, size):
-            result += recursive_calc(i, n)
+
+            if(i <= n + 1):
+                result += 1 / (i + n - 1)
 
         vector[i] = result
 
     return vector
 
-
+@timing_decorator
 def qr_decomp(matrix):
     q, r = np.linalg.qr(matrix)
     return q, r
 
-
+@timing_decorator
 def qr_back_substitution(q, r, b):
 
     qTb = np.matmul(q.T, b)
+    size = qTb.size
+    x = np.zeros(size)
 
-    x = np.linalg.solve(r, qTb)
+    for n in range(size - 1, 0, -1):
+        x[n] = (qTb[n] - np.dot(r[n, n:size], x[n:size])) / r[n,n]
 
     return x
 
+def qr_calc(size):
 
-def main():
-    size = 100
+    time = 0
+    exec_run = 0
 
     matrix = assemble_matrix(size)
+    exec_run = len(assemble_matrix.execution_times) - 1
+    time += assemble_matrix.execution_times[exec_run]
+
     q, r = qr_decomp(matrix)
+    time += qr_decomp.execution_times[exec_run]
 
     b = assemble_vector(size)
+    time += assemble_vector.execution_times[exec_run]
 
     x = qr_back_substitution(q, r, b)
+    time += qr_back_substitution.execution_times[exec_run]
 
-    print(x)
+    return x, time
+
+def main():
+
+    qr_calc(300)
+
+    n_values = np.arange(100, 10001, 50)
+    n_exec_time = np.zeros(n_values.size)
+
+    for n in range(0, n_values.size):
+        matrix, time = qr_calc(n_values[n])
+        print(n_values[n])
+        n_exec_time[n] = time
+    
+
+    print(n_values)
+    print(n_exec_time)
+
+    plt.plot(n_values, n_exec_time)
+    plt.ylabel('Execution Time')
+    plt.xlabel('Size of N')
+    plt.savefig('Aufgabe1_ExecutionTimePlot_Kaloyan_Kondov_2360001_Marlon_Schnell_2307973.png')
+    plt.show()
 
 
 if __name__ == "__main__":
--- svd_compression.py
+++ svd_compression.py
@@ -2,10 +2,13 @@ import numpy as np
 from scipy import misc
 import matplotlib.pyplot as plt
 from PIL import Image
+import math
 
 
-def k_approx(k, u, s, vh):
-    s_new = [0] * s.size
+def k_approx(k, matrix):
+    u, s, vh = np.linalg.svd(matrix)
+
+    s_new = np.zeros(s.size)
     for i in range(0, k):
         s_new[i] = s[i]
 
@@ -14,18 +17,52 @@ def k_approx(k, u, s, vh):
     return matrix
 
 
-def visualize_image(matrix):
+def visualize_image(matrix, name):
     im = Image.fromarray(matrix)
+    im = im.convert("L")
     im.show()
+    im.save(name)
+
+def calculate_frobenius(k, matrix):
+    k_matrix = k_approx(k, matrix)
+    
+    approx_error = 0
+    dimension = matrix.shape
+
+    for i in range(0, dimension[0] - 1):
+        for j in range(0, dimension[1] - 1):
+
+            approx_error += (matrix[i][j] - k_matrix[i][j]) ** 2
+    
+    approx_error = math.sqrt(approx_error)
+
+    return approx_error
+
 
 
 def main():
     image = misc.ascent()
+    print(np.linalg.matrix_rank(image))
     u, s, vh = np.linalg.svd(image)
 
-    visualize_image(k_approx(5, u, s, vh))
-    visualize_image(k_approx(20, u, s, vh))
-    visualize_image(k_approx(75, u, s, vh))
+    visualize_image(k_approx(5, image), "Aufgabe2_5_approx_Kaloyan_Kondov_2360001_Marlon_Schnell_2307973.png")
+    visualize_image(k_approx(20, image), "Aufgabe2_20_approx_Kaloyan_Kondov_2360001_Marlon_Schnell_2307973.png")
+    visualize_image(k_approx(75, image), "Aufgabe2_75_approx_Kaloyan_Kondov_2360001_Marlon_Schnell_2307973.png")
+
+    k_values = np.arange(1, np.linalg.matrix_rank(image))
+    k_approx_errors = np.zeros(k_values.size)
+
+    for n in range(0, k_values.size - 1):
+        k_approx_errors[n] = calculate_frobenius(k_values[n], image)
+        print(k_values[n])
+
+    print(k_approx_errors)
+
+    plt.plot(k_values, k_approx_errors)
+    plt.xlabel("Values for k")
+    plt.ylabel("Approximation Error")
+    plt.savefig('Aufgabe2_ApproximationErrorGraph__Kaloyan_Kondov_2360001_Marlon_Schnell_2307973.png')
+    plt.show()
 
 
 if __name__ == "__main__":
